8971ad42e732188ef3d9586af4cf29c35801f319
Matthew Blanchard <matthewblanc@msn.com>
2023-03-16 13:49:30 -0700

    Added a wand of polymorph.

---
+++ actors/equipment/wand/wand_of_polymorph.lua
@@ -0,0 +1,39 @@
+local Actor = require "actor"
+local Tiles = require "tiles"
+
+local Zap = actions.Zap:extend()
+Zap.name = "zap"
+Zap.targets = {targets.Item}
+
+function Zap:perform(level)
+    actions.Zap.perform(self, level)
+    
+    local zapper = self.owner
+    level:removeActor(zapper)
+
+    local spider = actors.Webweaver()
+    spider.position = zapper.position:copy()
+
+    local condition = conditions.Polymorph(zapper)
+    condition:setDuration(1000)
+    spider:applyCondition(condition)
+    level:addActor(spider)
+end
+
+-- Actual item definition all the way down here
+local WandOfLight = Actor:extend()
+WandOfLight.name = "Wand of Polymorph"
+WandOfLight.color = {1, 0.753, 0.796, 1}
+WandOfLight.char = Tiles["wand_gnarly"]
+
+WandOfLight.components = {
+    components.Item{stackable = false},
+    components.Usable(),
+    components.Edible{nutrition = 5},
+    components.Wand{
+        maxCharges = 3,
+        zap = Zap
+    },
+}
+
+return WandOfLight
