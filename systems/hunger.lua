73e68fe8bc0f78cd019f81a4832b6c28c52a2321
Matthew Blanchard <matthewblanc@msn.com>
2023-03-17 13:22:34 -0400

    Fixed HungerSystem name not being unique.

--- systems/hunger.lua
+++ systems/hunger.lua
@@ -1,7 +1,7 @@
 local System = require "system"
 
 local Hunger = System:extend()
-Hunger.name = "Message"
+Hunger.name = "Hunger"
 
 function Hunger:onTick(level, actor, action)
     for _, hunger_component in level:eachActor(components.Hunger) do
