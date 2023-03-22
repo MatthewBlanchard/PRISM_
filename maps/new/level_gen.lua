1be1f96cd05d72b711e83e3ee53a5cbf8db204c7
Diminim <diminiminim@gmail.com>
2023-03-22 02:54:53 -0600

    Merge branch 'proc_gen' of https://github.com/MatthewBlanchard/PRISM_private into proc_gen

--- maps/new/level_gen.lua
+++ maps/new/level_gen.lua
@@ -1,11 +1,9 @@
 --love.math.setRandomSeed(1)
-love.audio.setVolume(0)
+--love.audio.setVolume(0)
 
 local Map = require "maps.map"
 local Object = require "object"
 local vec2 = require "vector"
-local Clipper = require('maps.clipper.clipper')
-
 local Clipper = require('maps.clipper.clipper')
 
 local Level = Object:extend()
@@ -49,172 +47,234 @@ function Level:create(callback)
     end
   end
 
-
-
-
-  --[[
-  loop[2] = graph:new_node{
+  local prototype = graph:new_node{
     width = 4, height = 4,
-    actors = {
-      {name = 'Glowshroom', positioning = 'Center'}
-    }
+    shaper = function(params, room)
+      room:clear_rect(1,1, params.width-1, params.height-1)
+    end,
+    populater = function(params, room)
+    end,
   }
-  graph:add_node(loop[2])
 
-  loop[3] = graph:new_node{
+  local start = graph:new_node{
     width = 4, height = 4,
-    actors = {
-      {name = 'Stationarytorch', positioning = 'Center'}
-    }
+    shaper = function(params, room)
+      local cx, cy = room:get_center()
+      room:clear_ellipse(cx, cy, 1, 1)
+    end,
+    populater = function(params, room)
+      local cx, cy = room:get_center()
+      room:insert_actor('Player', cx, cy)
+    end,
   }
-  graph:add_node(loop[3])
-  --]]
+  graph:add_node(start)
 
-  -- local min, max = 4, 8
+  local finish = graph:new_node{
+    width = 4, height = 4,
+    shaper = function(params, room)
+      room:clear_rect(1,1, params.width-1, params.height-1)
+    end,
+    populater = function(params, room)
+      local cx, cy = room:get_center()
 
-  -- local loop = {}
-  -- loop[1] = graph:new_node{
-  --   width = love.math.random(min, max), height = love.math.random(min, max),
-  --   actors = {
-  --     {name = 'Player', positioning = 'Center'},
-  --     {name = 'Web', positioning = 'Center'},
-  --   }
+      room:insert_actor('Stairs', cx, cy)
+    end,
+  }
+  graph:add_node(finish)
+
+  -- local sqeeto_hive = graph:new_node{
+  --   width = 20, height = 20,
+  --   shaper = function(params, room)
+  --     room:clear_ellipse(params.width/2, params.height/2, 5, 5)
+  --     for i = 1, 20 do
+  --       room:DLAInOut()
+  --     end
+  --   end,
+  --   populater = function(params, room, clipping)
+  --     local cx, cy = math.floor(params.width/2)+1, math.floor(params.height/2)+1
+
+  --     for i = 1, 3 do
+  --       local x, y
+  --       repeat
+  --         x, y = love.math.random(1, params.width-1)+1, love.math.random(1, params.height-1)+1
+  --       until Clipper.PointInPolygon(Clipper.IntPoint(x, y), clipping) == 1
+  --       room:insert_actor('Sqeeto', x, y)
+  --     end
+
+  --     room:insert_actor('Prism', cx, cy)
+  --   end,
+
   -- }
-  -- graph:add_node(loop[1])
+  -- graph:add_node(sqeeto_hive)
 
-  -- for i = 2, 3 do
-  --   loop[i] = graph:new_node{
-  --     width = love.math.random(min, max), height = love.math.random(min, max),
-  --     actors = {
-  --       --{name = 'Stairs', positioning = 'Ce'}
-  --     }
-  --   }
-  --   graph:add_node(loop[i])
-  -- end
+  -- local spider_nest = graph:new_node{
+  --   width = 20, height = 20,
+  --   shaper = function(params, room)
+  --     for i = 1, 2 do
+  --       room:drunkWalk(room.width/2, room.height/2,
+  --         function(x, y, i, room)  
+  --           return (i > 10) or (x < 5 or x > room.width-5 or y < 5 or y > room.height-5)
+  --         end
+  --       )
+  --     end
 
-  -- loop[#loop] = graph:new_node{
-  --   width = love.math.random(min, max), height = love.math.random(min, max),
-  --   actors = {
-  --     {name = 'Stairs', positioning = 'Center'}
-  --   }
+  --     for i = 1, 20 do
+  --       room:DLA()
+  --     end
+  --   end,
+  --   populater = function(params, room, clipping)
+  --     local cx, cy = math.floor(params.width/2)+1, math.floor(params.height/2)+1
+
+  --     for i = 1, 1 do
+  --       local x, y
+  --       repeat
+  --         x, y = love.math.random(1, params.width-1)+1, love.math.random(1, params.height-1)+1
+  --       until Clipper.PointInPolygon(Clipper.IntPoint(x, y), clipping) == 1
+  --       room:insert_actor('Webweaver', x, y)
+  --     end
+
+  --     for i = 1, 1 do
+  --       local x, y
+  --       repeat
+  --         x, y = love.math.random(1, params.width-1)+1, love.math.random(1, params.height-1)+1
+  --       until Clipper.PointInPolygon(Clipper.IntPoint(x, y), clipping) == 1
+  --       --room:insert_actor('Web', x, y)
+  --     end
+
+  --   end,
+
   -- }
-  -- graph:add_node(loop[#loop])
+  -- graph:add_node(spider_nest)
 
-  -- for i, v in ipairs(loop) do
-  --   graph:connect_nodes({type='Join'}, v, (loop[i+1] or loop[1] ))
-  -- end
+  -- local shop = graph:new_node{
+  --   width = 8, height = 8,
+  --   shaper = function(params, room)
+  --     room:clear_rect(1,1, params.width-1, params.height-1)
+  --   end,
+  --   populater = function(params, room)
+  --     local cx, cy = math.floor(params.width/2)+1, math.floor(params.height/2)+1
 
-  --[[
-      graph:add_node(uniques.finish)
-  uniques['challenge'] = graph:new_node{
-    width = 20, height = 20,
-    shaping = function(params, room)
-      for i = 1, 2 do
-        room:drunkWalk(room.width/2, room.height/2,
-          function(x, y, i, room)  
-            return (i > 10) or (x < 5 or x > room.width-5 or y < 5 or y > room.height-5)
-          end
-        )
-      end
+  --     local _, shopkeep_id = room:insert_actor('Shopkeep', cx, cy-1)
+  --     room:insert_actor('Stationarytorch', cx-2, cy-1)
+  --     room:insert_actor('Stationarytorch', cx+2, cy-1)
 
-      for i = 1, 20 do
-        room:DLA()
-      end
-    end,
-    actors = {
-      {name = 'Webweaver', positioning = 'Center'}
-    }
-  }
-  ]]
-
-  local uniques = {}
-  uniques['start'] = graph:new_node{
-    width = 4, height = 4,
-    shaping = function(params, room)
-      clear_ellipse(params.width/2, params.height/2, 1, 1)(params, room)
-    end,
-    actors = {
-      {name = 'Player', positioning = 'Center'}
-    }
-  }
-  graph:add_node(uniques.start)
-  uniques['finish'] = graph:new_node{
-    width = 4, height = 4, 
-    actors = {
-      {name = 'Stairs', positioning = 'Center'}
-    }
-  }
-  graph:add_node(uniques.finish)
-  uniques['challenge'] = graph:new_node{
-    width = 20, height = 20,
-    shaping = function(params, room)
-      for i = 1, 2 do
-        room:drunkWalk(room.width/2, room.height/2,
-          function(x, y, i, room)  
-            return (i > 10) or (x < 5 or x > room.width-5 or y < 5 or y > room.height-5)
-          end
-        )
-      end
-
-      for i = 1, 20 do
-        room:DLA()
-      end
-    end,
-  }
-  graph:add_node(uniques.challenge)
-
-  local branches = {}
-  branches['reward'] = graph:new_node{
-    width = 4, height = 4, 
-    actors = {
-      {name = 'Prism', positioning = 'Center'}
-    }
-  }
-  --graph:add_node(branches.reward)
-
-  -- local filler = {}
-  -- for i = 1, 5 do
-  --   filler[i] = graph:new_node{
-  --     width = love.math.random(5, 10), height = love.math.random(5, 10), 
-  --     actors = {
+  --     local shopItems = {
+  --       {
+  --         components.Weapon,
+  --         components.Wand
+  --       },
+  --       {
+  --         components.Equipment
+  --       },
+  --       {
+  --         components.Edible,
+  --         components.Drinkable,
+  --         components.Readable
+  --       }
   --     }
-  --   }
-  --   graph:add_node(filler[i])
-  -- end
 
-  -- for i, v in ipairs(filler) do
-  --   local random_connection_index
-  --   while true do
-  --     random_connection_index = love.math.random(1, #filler)
+  --     for i = 1, 3 do
+  --       local itemTable = shopItems[i]
+  --       local item = Loot.generateLoot(itemTable[love.math.random(1, #itemTable)])
 
-  --     local already_connected = false
-  --     for i2, v2 in ipairs(v.edges) do
-  --       if v2 == filler[random_connection_index] then
-  --         already_connected = true
-  --         break
+  --       local callback = function(actor, actors_by_unique_id)
+  --         local status = ''
+
+  --         local sellable_component = actor:getComponent(components.Sellable)
+  --         sellable_component:setItem(item)
+  --         sellable_component:setPrice(actors.Shard, item:getComponent(components.Cost).cost)
+
+  --         if actors_by_unique_id[shopkeep_id] then
+  --           sellable_component:setShopkeep(actors_by_unique_id[shopkeep_id])
+  --         else
+  --           status = 'delay'
+  --         end
+
+  --         return status
   --       end
+  --       room:insert_actor('Product', cx-2+i, cy, callback)
   --     end
+  --   end,
+  -- }
+  -- graph:add_node(shop)
 
-  --     if
-  --       (graph.nodes[filler[random_connection_index]] ~= v) and
-  --       (already_connected == false)
-  --     then
-  --       break
-  --     end
-  --   end
-  --   graph:connect_nodes({type='Join'}, v, filler[random_connection_index])
-  -- end
+  -- local snip_farm = graph:new_node{
+  --   width = 10, height = 10,
+  --   shaper = function(params, room)
+  --     room:clear_rect(1,1, params.width-1, params.height-1)
+  --   end,
+  --   populater = function(params, room)
+  --     local cx, cy = math.floor(params.width/2)+1, math.floor(params.height/2)+1
 
-  -- for k, v in pairs(uniques) do
-  --   graph:connect_nodes({type='Join'}, v, filler[love.math.random(1, #filler)])
-  -- end
+  --     room:fill_perimeter(cx-2, cy-2, cx+2, cy+2)
+  --     room:clear_cell(cx, cy+2)
 
+  --     local _, shopkeep_id = room:insert_actor('Shopkeep', cx, cy+2)
 
+  --     room:target_rect(cx-1, cy-1, cx+1, cy+1, function(x, y)
+  --         room:insert_actor('Snip', x, y)
+  --       end
+  --     )
+  --   end,
+  -- }
+  -- graph:add_node(snip_farm)
 
-  graph:connect_nodes({type='Join'}, uniques.start, uniques.challenge)
-  graph:connect_nodes({type='Join'}, uniques.challenge, uniques.finish)
+  -- local tunnel = graph:new_node{
+  --   width = 10, height = 10,
+  --   shaper = function(params, room)
+  --     local cx, cy = room:get_center()
 
-  --graph:connect_nodes({type='Join'}, uniques.challenge, branches.reward)
+  --     room:drunkWalk(cx, cy,
+  --       function(x, y, i, room)  
+  --         return (i > 10) or (x < 1 or x > room.width-1 or y < 1 or y > room.height-1)
+  --       end
+  --     )
+
+  --   end,
+  --   populater = function(params, room)
+  --   end,
+  -- }
+  -- graph:add_node(tunnel)
+
+  --graph:connect_nodes({type = 'Join'}, start, big, finish)
+
+  local filler_nodes = {}
+  for i = 1, 4 do
+    filler_nodes[i] = graph:new_node{
+      width = love.math.random(4, 10), height = love.math.random(4, 10),
+      shaper = function(params, room)
+        room:clear_rect(1,1, params.width-1, params.height-1)
+      end,
+      populater = function(params, room)
+        local cx, cy = math.floor(params.width/2)+1, math.floor(params.height/2)+1
+      end,
+    }
+    graph:add_node(filler_nodes[i])
+
+    if i > 1 then
+      local tunnel = graph:new_node{
+        width = 15, height = 15,
+        shaper = function(params, room)
+          local cx, cy = room:get_center()
+    
+          room:drunkWalk(cx, cy,
+            function(x, y, i, room)  
+              return (i > 10) or (x < 1 or x > room.width-1 or y < 1 or y > room.height-1)
+            end
+          )
+    
+        end,
+        populater = function(params, room)
+        end,
+      }
+      graph:add_node(tunnel)
+
+      graph:connect_nodes({type = 'Join'}, filler_nodes[i], tunnel, filler_nodes[love.math.random(1, i-1)])
+    end
+  end
+
+  graph:connect_nodes({type = 'Join'}, start, filler_nodes[love.math.random(1, #filler_nodes)])
+  graph:connect_nodes({type = 'Join'}, finish, filler_nodes[love.math.random(1, #filler_nodes)])
 
 
   local merged_room_3 = Map:special_merge(graph)
