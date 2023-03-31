90c643c2f80d33a3173370d21044af950ba6410e
Diminim <diminiminim@gmail.com>
2023-03-31 09:51:59 -0600

    renamed graph.room to graph.chunk

--- maps/chunks/start.lua
+++ maps/chunks/start.lua
@@ -4,16 +4,16 @@ local start = Chunk:extend()
 function start:parameters()
   self.width, self.height = 4, 4
 end
-function start:shaper(map)
-  local cx, cy = map:get_center()
-  map:clear_ellipse(cx, cy, 1, 1)
+function start:shaper(chunk)
+  local cx, cy = chunk:get_center()
+  chunk:clear_ellipse(cx, cy, 1, 1)
 end
-function start:populater(map, clipping)
-  local cx, cy = map:get_center()
-  map:insert_actor('Player', cx, cy)
-  map:insert_actor('Wand_of_blastin', cx, cy+1)
-  map:insert_actor('Key_type', cx-1, cy)
-  map:insert_actor('Key_id', cx, cy-1, nil, self.key_id)
+function start:populater(chunk, clipping)
+  local cx, cy = chunk:get_center()
+  chunk:insert_actor('Player', cx, cy)
+  chunk:insert_actor('Wand_of_blastin', cx, cy+1)
+  chunk:insert_actor('Key_type', cx-1, cy)
+  chunk:insert_actor('Key_id', cx, cy-1, nil, self.key_id)
   
   local callback = function(actor, actors_by_unique_id)
     local chest_inventory = actor:getComponent(components.Inventory)
@@ -21,7 +21,7 @@ function start:populater(map, clipping)
     local chest_lock = actor:getComponent(components.Lock_type)
     chest_lock:setKey(actors.Key_type)
   end
-  map:insert_actor('Chest_lock_type', cx+1, cy, callback)
+  chunk:insert_actor('Chest_lock_type', cx+1, cy, callback)
 end
 
 return start
