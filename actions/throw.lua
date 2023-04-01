8a3dbc6987c404258d617beedf33c739bac869e7
Matthew Blanchard <matthewblanc@msn.com>
2023-04-01 11:44:36 -0400

    Made the throw action more sophisticated.

--- actions/throw.lua
+++ actions/throw.lua
@@ -1,4 +1,6 @@
 local Action = require "action"
+local Bresenham = require "bresenham"
+local Vector2 = require "vector"
 
 local ThrowTarget = targets.Point:extend()
 ThrowTarget.name = "throwtarget"
@@ -13,9 +15,19 @@ function Throw:perform(level)
   local thrown = self.targetActors[1]
   local point = self.targetActors[2]
 
-  local effects_system = level:getSystem("Effects")
-  effects_system:addEffect(effects.throw(thrown, self.owner, point))
-  level:moveActor(thrown, point)
+  local ox, oy = self.owner.position.x, self.owner.position.y
+  local px, py = point.x, point.y
+  local line, valid = Bresenham.line(ox, oy, px, py)
+
+  for i = 2, #line do
+    local point = line[i]
+    if level:getCellPassable(point[1], point[2]) then
+      level:moveActor(thrown, Vector2(point[1], point[2]))
+      coroutine.yield("wait", 0.1)
+    else
+      return
+    end
+  end
 end
 
 return Throw
