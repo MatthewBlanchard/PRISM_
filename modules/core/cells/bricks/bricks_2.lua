43681ff605ed4fb5c3039086112a51b42252dc78
Diminim <diminiminim@gmail.com>
2023-04-09 14:01:11 -0600

    added new cell types and hooked in the cell:onAction callback

---
+++ modules/core/cells/bricks/bricks_2.lua
@@ -0,0 +1,10 @@
+local Cell = require "core.cell"
+local Tiles = require "display.tiles"
+
+local Bricks = Cell:extend()
+Bricks.name = "Bricks"
+Bricks.passable = false
+Bricks.opaque = true
+Bricks.tile = Tiles["wall_2"]
+
+return Bricks
