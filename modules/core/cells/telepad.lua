95fc9b44379022a9cae649359454322d19e3af04
Diminim <diminiminim@gmail.com>
2023-04-09 23:23:04 -0600

    caught up with planar embedding

--- modules/core/cells/telepad.lua
+++ modules/core/cells/telepad.lua
@@ -5,7 +5,6 @@ local Telepad = Cell:extend()
 Telepad.name = "Telepad"
 Telepad.passable = true
 Telepad.opaque = false
-Telepad.sightLimit = nil
 Telepad.tile = Tiles["circle_1"]
 --Telepad.teleport_destination = nil
 
