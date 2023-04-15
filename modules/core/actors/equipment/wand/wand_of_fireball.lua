ea1eb5cb779020a860a34e34f9314e11ef445967
Matthew Blanchard <matthewblanc@msn.com>
2023-04-15 17:27:17 -0400

    Wand of fireball now lights up it's path/explosion.

--- modules/core/actors/equipment/wand/wand_of_fireball.lua
+++ modules/core/actors/equipment/wand/wand_of_fireball.lua
@@ -3,15 +3,23 @@ local Action = require "core.action"
 local Condition = require "core.condition"
 local Tiles = require "display.tiles"
 local Color = require "math.color"
+local LightColor = require "structures.lighting.lightcolor"
+local Bresenham = require "math.bresenham"
+local Vector2 = require "math.vector"
 
-local function FireballLightEffect(x, y, duration)
-    local t = 0
-    return function (dt)
-        t = t + dt
-        if t > duration then return nil end
-        return x, y, Color.mul({0.8, 0.8, 0.1}, (1 - t/duration)*2)
-    end
-end
+local fireballLight = Actor:extend()
+fireballLight.name = "Fireball"
+fireballLight.color = {1, 0.6, 0.2}
+fireballLight.char = Tiles["projectile1"]
+fireballLight.components = {
+  components.Light{
+    color = LightColor(28, 16, 1),
+    effect = {components.Light.effects.flicker, {0.15, 0.3}},
+  },
+  components.Animated{
+    sheet = { Tiles["projectile1"], Tiles["projectile2"] }
+  }
+}
 
 local ZapTarget = targets.Point:extend()
 ZapTarget.name = "ZapTarget"
@@ -19,20 +27,34 @@ ZapTarget.range = 9
 
 local Zap = actions.Zap:extend()
 Zap.name = "zap"
-Zap.aoeRange = 1
+Zap.aoeRange = 2
 Zap.targets = {targets.Item, ZapTarget}
 
 function Zap:perform(level)
-  actions.Zap.perform(self, level)
+  actions.Zap.perform(self, level, true)
 
-  local target = self.targetActors[2]
+  local pointTarget = self.targetActors[2]
 
-  local fov, actors = level:getAOE("fov", target, self.aoeRange)
+  local fov, actors = level:getAOE("fov", pointTarget, self.aoeRange)
   local damage = ROT.Dice.roll("2d6")
-  table.insert(level:getSystem("Lighting").__temporaryLights, FireballLightEffect(target.x, target.y, 0.6))
+
+  local light = fireballLight()
+  level:addActor(light)
+  level:moveActor(light, pointTarget)
+
+  local ox, oy = self.owner.position.x, self.owner.position.y
+  local px, py = pointTarget.x, pointTarget.y
+  local line, valid = Bresenham.line(ox, oy, px, py)
+
+  for i = 2, #line do
+    level:moveActor(light, Vector2(line[i][1], line[i][2]))
+    level:yield("wait", 0.1)
+  end
 
   local effects_system = level:getSystem("Effects")
-  effects_system:addEffect(level, effects.ExplosionEffect(fov, target, self.aoeRange))
+  effects_system:addEffect(level, effects.ExplosionEffect(fov, pointTarget, self.aoeRange))
+
+  level:removeActor(light)
 
   effects_system:suppressEffects()
   for i, actor in ipairs(actors) do
@@ -41,7 +63,7 @@ function Zap:perform(level)
       level:performAction(damage)
     end
   end
-  effects_system:resumeEffects()
+  effects_system:resumeEffects(level)
 end
 
 local WandOfFireball = Actor:extend()
