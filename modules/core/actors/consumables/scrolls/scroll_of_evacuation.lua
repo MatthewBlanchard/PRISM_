c50da0fbb5171d7ebe8948ea07d483773e5a9ef2
Liam <liamreadsspam@gmail.com>
2023-04-24 20:30:40 -0300

    Scroll of evacuation

---
+++ modules/core/actors/consumables/scrolls/scroll_of_evacuation.lua
@@ -0,0 +1,33 @@
+local Actor = require "core.actor"
+local Action = require "core.action"
+local Tiles = require "display.tiles"
+local Condition = require "core.condition"
+
+local Scrying = Condition:extend()
+Scrying.name = "Scrying"
+Scrying.damage = 1
+
+Scrying:onScry(function(self, level, actor) return { level:getActorByType(actors.Stairs ) } end)
+
+local Read = actions.Read:extend()
+Read.name = "read"
+Read.targets = { targets.Item }
+
+function Read:perform(level)
+   actions.Read.perform(self, level)
+   self.owner:applyCondition(Scrying())
+end
+
+local Scroll = Actor:extend()
+Scroll.name = "Scroll of Evacuation"
+Scroll.color = { 0.8, 0.8, 0.8, 1 }
+Scroll.char = Tiles["scroll"]
+
+Scroll.components = {
+   components.Item(),
+   components.Usable(),
+   components.Readable { read = Read },
+   components.Cost(),
+}
+
+return Scroll
